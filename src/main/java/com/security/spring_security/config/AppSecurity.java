149df66a4d5c76acc21caad603c8aac1168cc1f4
Jagadeesh Bandi <jagadeesh.bandi@cjsstechnologies.com>
2026-03-26 14:51:01 +0530

    security config update

--- src/main/java/com/security/spring_security/config/AppSecurity.java
+++ src/main/java/com/security/spring_security/config/AppSecurity.java
@@ -1,32 +1,29 @@
 package com.security.spring_security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurity {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     UserDetailsService userDetailsService(){
-        UserDetails adminUser = User.withUsername("Krishna")
-                .password(passwordEncoder().encode("Krishna"))
-                .roles("ADMIN").build();
-        UserDetails normalUser = User.withUsername("Raghu")
-                .password(passwordEncoder().encode("Raghu"))
-                .roles("USER").build();
-        return new InMemoryUserDetailsManager(adminUser,normalUser);
-
+    return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
